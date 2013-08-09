06d9f0d06df38b34ece2650fb6d9d0a7851aad95
Albert Deuzeman (ITP Bern) <deuzeman@itp.unibe.ch>
2013-08-09 12:03:42 +0200

    Draft implementation of gradient flow complete -- currently using direct forward integration, to be update to Runge-Kutta.

--- smearing/gradient_gradient_smear.c
+++ smearing/gradient_gradient_smear.c
@@ -2,39 +2,52 @@
 
 void gradient_smear(gradient_control *control, gauge_field_t in)
 {
-  /* We may alias the data, so we need something to store intermediate results somewhere else then m_field_out */
+  /* Allocate the required memory */
   control->U[0] = in;
-
-  su3 staples;
-  
-  /* We need to take staples, so we need some working memory... */
   gauge_field_t buffer = get_gauge_field();
-
-  double const coeff_principal = 1.0 - 6.0 * control->coeff;
-
+  su3 ALIGN staples;
+  su3 ALIGN tmp;
+  
+  /* Calculate the iteration number */
+  unsigned int steps = (int)floor(control->distance / control->epsilon);
+  double red_eps = control->distance - (steps * control->epsilon);
+  int remainder = (red_eps / epsilon) > 1e-6;
+  
   /* start of the the stout smearing **/
-#pragma omp parallel private(staples)
-  for(unsigned int iter = 0; iter < control->iterations; ++iter)
+#pragma omp parallel private(staples, tmp)
+  for(unsigned int iter = 0; iter < steps; ++iter)
   {
 #pragma omp for
     for (unsigned int x = 0; x < VOLUME; ++x)
       for (unsigned int mu = 0; mu < 4; ++mu)
       {
+        // Note the difference with Luescher's definition (0907.5491v3 p14):
+        // The orientation of the plaquette is inverted there and a minus sign is added.
+        // Since we project to the traceless antihermitian vector space, 
+        // we can leave out the minus sign.
+        // This brings it in line with our implementation of stout smearing, coincidentally.
         generic_staples(&staples, x, mu, in);
-        _real_times_su3_plus_real_times_su3(buffer[x][mu], coeff_principal, in[x][mu], control->coeff, staples);
-        reunitarize(&buffer[x][mu]);
+        _su3_times_su3d(tmp, staples, in[x][mu]);
+        project_traceless_antiherm(&tmp);        
+        _real_times_su3(tmp, control->epsilon, tmp);
+        exposu3_in_place(&tmp);
+        _su3_times_su3(buffer[x][mu], tmp, in[x][mu]);
       }
 
-#pragma omp single
+    /* Prepare for the next iteration, swap and exchange fields */
+    /* There should be an implicit OMP barrier here */
+
+    #pragma omp single
     {
-      /* Prepare for the next iteration -- the last result is now input! */
       swap_gauge_field(&control->U[1], &buffer);
-      exchange_gauge_field(&control->U[1]);
-      in = control->U[1];
+      exchange_gauge_field(&control->U[1]); /* The edge terms will have changed by now */
+      in = control->U[1]; /* Shallow copy intended */
     }
   }
 
   control->result = control->U[1];
-  
   return_gauge_field(&buffer);
 }
+
+
+  
