91b58829bcc812c989b778a6315fe86e8b51194c
Bartosz Kostrzewa <bartosz.kostrzewa@desy.de>
2016-05-06 14:28:18 +0200

    init_bsm_2hop_lookup: guard against multiple initialisations

--- init/init_bsm_2hop_lookup.c
+++ init/init_bsm_2hop_lookup.c
@@ -29,50 +29,55 @@
 #include "fatal_error.h"
 
 int init_bsm_2hop_lookup(const int V) {
-  g_bsm_2hop_lookup = malloc(V*32*sizeof(int));
+  static int bsm_2hop_lookup_initialised = 0;
   
-  if((void*)g_bsm_2hop_lookup == NULL) fatal_error("malloc failed","init_bsm_2hop_lookup");
-                                            
-  for(int ix = 0; ix < V; ++ix){
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P0  ] = g_iup[ix][0];
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP0 ] = g_iup[ g_iup[ix][0] ][0];
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_0   ] = ix;
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P0  ] = g_iup[ix][0];  
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M0  ] = g_idn[ix][0];             
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM0 ] = g_idn[ g_idn[ix][0] ][0]; 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M0  ] = g_idn[ix][0];
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM0 ] = g_idn[ g_idn[ix][0] ][0]; 
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P1  ] = g_iup[ix][1];                
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP1 ] = g_iup[ g_iup[ix][1] ][1];   
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_1   ] = ix;                         
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P1  ] = g_iup[ix][1];               
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M1  ] = g_idn[ix][1];               
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM1 ] = g_idn[ g_idn[ix][1] ][1];   
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M1  ] = g_idn[ix][1];              
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM1 ] = g_idn[ g_idn[ix][1] ][1];  
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P2  ] = g_iup[ix][2];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP2 ] = g_iup[ g_iup[ix][2] ][2];     
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_2   ] = ix;                           
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P2  ] = g_iup[ix][2];                 
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M2  ] = g_idn[ix][2];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM2 ] = g_idn[ g_idn[ix][2] ][2];     
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M2  ] = g_idn[ix][2];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM2 ] = g_idn[ g_idn[ix][2] ][2];    
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P3  ] = g_iup[ix][3];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP3 ] = g_iup[ g_iup[ix][3] ][3];     
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_3   ] = ix;                           
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P3  ] = g_iup[ix][3];                 
-
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M3  ] = g_idn[ix][3];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM3 ] = g_idn[ g_idn[ix][3] ][3];     
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M3  ] = g_idn[ix][3];                 
-    g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM3 ] = g_idn[ g_idn[ix][3] ][3];    
+  if( bsm_2hop_lookup_initialised == 0 ){
+    g_bsm_2hop_lookup = malloc(V*32*sizeof(int));
+    
+    if((void*)g_bsm_2hop_lookup == NULL) fatal_error("malloc failed","init_bsm_2hop_lookup");
+                                              
+    for(int ix = 0; ix < V; ++ix){
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P0  ] = g_iup[ix][0];
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP0 ] = g_iup[ g_iup[ix][0] ][0];
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_0   ] = ix;
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P0  ] = g_iup[ix][0];  
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M0  ] = g_idn[ix][0];             
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM0 ] = g_idn[ g_idn[ix][0] ][0]; 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M0  ] = g_idn[ix][0];
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM0 ] = g_idn[ g_idn[ix][0] ][0]; 
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P1  ] = g_iup[ix][1];                
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP1 ] = g_iup[ g_iup[ix][1] ][1];   
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_1   ] = ix;                         
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P1  ] = g_iup[ix][1];               
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M1  ] = g_idn[ix][1];               
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM1 ] = g_idn[ g_idn[ix][1] ][1];   
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M1  ] = g_idn[ix][1];              
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM1 ] = g_idn[ g_idn[ix][1] ][1];  
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P2  ] = g_iup[ix][2];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP2 ] = g_iup[ g_iup[ix][2] ][2];     
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_2   ] = ix;                           
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P2  ] = g_iup[ix][2];                 
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M2  ] = g_idn[ix][2];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM2 ] = g_idn[ g_idn[ix][2] ][2];     
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M2  ] = g_idn[ix][2];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM2 ] = g_idn[ g_idn[ix][2] ][2];    
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_P3  ] = g_iup[ix][3];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_PP3 ] = g_iup[ g_iup[ix][3] ][3];     
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_3   ] = ix;                           
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_P3  ] = g_iup[ix][3];                 
+  
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_M3  ] = g_idn[ix][3];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_S_MM3 ] = g_idn[ g_idn[ix][3] ][3];     
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_M3  ] = g_idn[ix][3];                 
+      g_bsm_2hop_lookup[32*ix + BSM_2HOP_U_MM3 ] = g_idn[ g_idn[ix][3] ][3];    
+    }
+    bsm_2hop_lookup_initialised = 1;
   }
 
   return(0);
