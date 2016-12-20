2410f09a378e9f18b24c0f6b4293960522100d13
Ferenc Pittler <pittlerferenc@gmail.com>
2016-12-20 17:34:12 +0100

    utils_nogauge.h corrected

--- buffers/utils_nogauge.h
+++ buffers/utils_nogauge.h
@@ -1,9 +1,10 @@
+#ifndef _UTILS_NOGAUGE_H
+#define _UTILS_NOGAUGE_H
+
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
-#ifndef _UTILS_NOGAUGE_H
-#define _UTILS_NOGAUGE_H
 #include <global.h>
 void generic_exchange_nogauge(void *field_in, int bytes_per_site );
 #endif
